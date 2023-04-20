34fcc4bebf8773a72c0e99dd43c8e8280c160d74
calabaraburus <calabaraburus@gmail.com>
2023-04-20 17:48:01 +0500

    added level constructor class

---
+++ assets/entities/configuration/LevelConfiguration.ts
@@ -0,0 +1,14 @@
+import { _decorator, Component, Node } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('LevelConfiguration')
+export class LevelConfiguration extends Component {
+    start() {
+
+    }
+
+    update(deltaTime: number) {
+        
+    }
+}
+
