6cf5e56a09c40b8fb3693e866730020de500a39b
artnatan <artnatan74@gmail.com>
2023-05-17 12:39:53 +0300

    update arena, update infoWindows, complete levels

--- assets/entities/infoPanel/PlayerInfoWindow.ts
+++ assets/entities/infoPanel/PlayerInfoWindow.ts
@@ -1,15 +1,34 @@
 import { BonusModel } from "../../models/BonusModel";
 import { PlayerModel } from "../../models/PlayerModel";
+import { CardInfoService } from "./CardInfoService";
 import { PopupWindow } from "./PopupWindow";
-import { Label, Sprite, SpriteFrame, _decorator, find } from "cc";
+import {
+  Graphics,
+  Label,
+  Sprite,
+  SpriteFrame,
+  _decorator,
+  Node,
+  Button,
+  EventHandler,
+  Prefab,
+  instantiate,
+  Vec3,
+} from "cc";
 const { ccclass, property } = _decorator;
 
 @ccclass("PlayerInfoWindow")
 export class PlayerInfoWindow extends PopupWindow {
+  @property(Prefab)
+  cardPrefab: Prefab;
+
   public setPlayer(playerModel: PlayerModel) {
     const playerName = playerModel.playerName;
     const heroImage = playerModel.heroImage;
     const bonuses = playerModel.bonuses;
+    const cardBlock = this.node.getChildByPath("Cards");
+
+    if (cardBlock?.children.length != 0) return;
 
     const componentName = this.node
       .getChildByName("NamePlayer")
@@ -24,25 +43,16 @@ export class PlayerInfoWindow extends PopupWindow {
     componentHeroImage.spriteFrame = heroImage;
 
     //bonuses
-    const componentBonuseList = [
-      this.node
-        .getChildByPath("Cards/GraphicsCardOne/BonusCard")
-        ?.getComponent(Sprite),
-      this.node
-        .getChildByPath("Cards/GraphicsCardTwo/BonusCard")
-        ?.getComponent(Sprite),
-      this.node
-        .getChildByPath("Cards/GraphicsCardThree/BonusCard")
-        ?.getComponent(Sprite),
-    ];
+    bonuses.forEach((bonus) => {
+      const card = instantiate(this.cardPrefab);
+      card.parent = cardBlock;
+      card.getComponent(CardInfoService)?.setModel(bonus);
 
-    for (let i = 0; i < componentBonuseList.length; i++) {
-      if (
-        componentBonuseList[i] != null ||
-        componentBonuseList[i] != undefined
-      ) {
-        componentBonuseList[i]!.spriteFrame = bonuses[i].cardImage;
-      }
-    }
+      const cardSprite = card
+        .getChildByName("spriteFrame")
+        ?.getComponent(Sprite);
+      if (!cardSprite) return;
+      cardSprite.spriteFrame = bonus.cardImage;
+    });
   }
 }
