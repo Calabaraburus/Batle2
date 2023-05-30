f67ab5c11d149033c6d6af91792606c4f9082f3b
artnatan <artnatan74@gmail.com>
2023-05-30 22:25:02 +0300

    update audio module, add music to menu

--- assets/entities/menu/MenuSelectorController.ts
+++ assets/entities/menu/MenuSelectorController.ts
@@ -1,17 +1,36 @@
-import { _decorator, director } from "cc";
+import { AudioSource, SpriteFrame, _decorator, director } from "cc";
 import { MainMenu } from "./MainMenu";
 import { LoaderScreen } from "./LoaderScreen";
+import { AudioManager } from "../../soundsPlayer/AudioManager";
 const { ccclass, property } = _decorator;
 
 @ccclass("MenuSelectorController")
 export class MenuSelectorController extends MainMenu {
   private _tarnsitionScene = new LoaderScreen();
+  private _aManager: AudioManager | null;
   menuSections = ["mainMenuLayout", "gameMenuLayout", "optionsMenuLayout"];
 
+  start(): void {
+    if (this._aManager == null) return;
+    this._aManager.playMusic("epic");
+
+    // const audioManager = director.getScene()?.getChildByName("__audioMgr__");
+    // if (!audioManager) return;
+    // if (!this._aManager) return;
+    // const aSours = audioManager.getComponent(AudioSource)
+
+    // if (aSours == null) return;
+    // aSours.clip = this._aManager.music[0];
+  }
+
+  onLoad(): void {
+    this._aManager = this.getComponent(AudioManager);
+  }
+
   openSectionMenu(sender: object, sectionMenu: string): void {
-    this.menuSections.forEach((i) => {
-      if (i != sectionMenu) {
-        const menuFrom = this.node.getChildByName(i);
+    this.menuSections.forEach((name) => {
+      if (name != sectionMenu) {
+        const menuFrom = this.node.getChildByName(name);
         if (menuFrom != null) {
           menuFrom.active = false;
         }
@@ -27,5 +46,15 @@ export class MenuSelectorController extends MainMenu {
 
   loadScene(sender: object, sceneName: string): void {
     director.loadScene(sceneName);
+  }
+
+  settingOptions(sender: object, managerName: string) {
+    // const audioManager = director.getScene()?.getChildByName("__audioMgr__");
+
+    if (managerName == "music") {
+      this._aManager?.changeVolume(this._aManager?.audioSource);
+    } else if (managerName == "sound") {
+      this._aManager?.changeVolume(this._aManager?.soundSource);
+    }
   }
 }
