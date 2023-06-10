7eaf82c447036027137ac4c77dc372170be0ca2a
calabaraburus <calabaraburus@gmail.com>
2023-06-11 02:48:59 +0500

    Fixed balista effect bug

--- assets/entities/effects/BalistaCardEffect.ts
+++ assets/entities/effects/BalistaCardEffect.ts
@@ -6,32 +6,9 @@ const { ccclass, property } = _decorator;
 export class BalistaCardEffect extends CardEffect {
   private isplaying = false;
 
-  @property({ type: ParticleSystem })
-  systems: ParticleSystem[] = [];
-
   @property({ type: Node })
   arrowSpriteNode: Node;
 
   @property({ type: Node })
   aim: Node;
-
-  play() {
-    this.isplaying = true;
-    this.systems.forEach((s) => {
-      s.play();
-    });
-  }
-
-  stop() {
-    this.isplaying = false;
-    this.systems.forEach((s) => {
-      s.stop();
-    });
-  }
-
-  stopEmmit() {
-    this.systems.forEach((s) => {
-      s.stopEmitting();
-    });
-  }
 }
