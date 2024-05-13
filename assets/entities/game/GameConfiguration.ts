dce3900591c2222a0854f0cf831619d90217d7db
calabaraburus <calabaraburus@gmail.com>
2024-05-13 14:14:59 +0300

    added victory and lose sound effects

--- assets/entities/game/GameConfiguration.ts
+++ assets/entities/game/GameConfiguration.ts
@@ -55,7 +55,7 @@ export class GameConfigurationModel {
       lvl.lvlName = "lvl4";
       lvl.botHeroName = "bot4";
       lvl.botLife = "40";
-      lvl.botCards = [{ mnemonic: "hammerMiddle", price: "4" }, { mnemonic: "mine", price: "3" }];
+      lvl.botCards = [{ mnemonic: "hammerMiddle", price: "4" }, { mnemonic: "totemLow", price: "3" }];
       lvl.playerHeroName = "rezkar";
       lvl.playerLife = "";
       lvl.playerCards = [];
@@ -68,7 +68,7 @@ export class GameConfigurationModel {
       lvl.lvlName = "lvl5";
       lvl.botHeroName = "bot5";
       lvl.botLife = "50";
-      lvl.botCards = [{ mnemonic: "wormMiddle", price: "4" }, { mnemonic: "totemLow", price: "3" }, { mnemonic: "push", price: "3" }];
+      lvl.botCards = [{ mnemonic: "wormMiddle", price: "4" }, { mnemonic: "mine", price: "3" }, { mnemonic: "push", price: "3" }];
       lvl.playerHeroName = "rezkar";
       lvl.playerLife = "";
       lvl.playerCards = [];
@@ -80,7 +80,7 @@ export class GameConfigurationModel {
       lvl.lvlName = "lvl6";
       lvl.botHeroName = "bot6";
       lvl.botLife = "70";
-      lvl.botCards = [{ mnemonic: "pikeLow", price: "3" }, { mnemonic: "shaman", price: "4" }, { mnemonic: "push", price: "4" }];
+      lvl.botCards = [{ mnemonic: "pikeMiddle", price: "3" }, { mnemonic: "shamanLow", price: "3" }, { mnemonic: "maneuver", price: "4" }];
       lvl.playerHeroName = "rezkar";
       lvl.playerLife = "";
       lvl.playerCards = [];
@@ -92,7 +92,7 @@ export class GameConfigurationModel {
       lvl.lvlName = "lvl7";
       lvl.botHeroName = "bot7";
       lvl.botLife = "80";
-      lvl.botCards = [{ mnemonic: "meteoriteMiddle", price: "4" }, { mnemonic: "totem", price: "4" }, { mnemonic: "maneuver", price: "3" }];
+      lvl.botCards = [{ mnemonic: "meteoriteMiddle", price: "4" }, { mnemonic: "berserk", price: "4" }, { mnemonic: "shield", price: "2" }];
       lvl.playerHeroName = "rezkar";
       lvl.playerLife = "";
       lvl.playerCards = [];
@@ -116,7 +116,7 @@ export class GameConfigurationModel {
       lvl.lvlName = "lvl9";
       lvl.botHeroName = "bot9";
       lvl.botLife = "90";
-      lvl.botCards = [{ mnemonic: "firewall", price: "4" }, { mnemonic: "assassin", price: "4" }, { mnemonic: "panic", price: "3" }];
+      lvl.botCards = [{ mnemonic: "lightning", price: "4" }, { mnemonic: "catapult", price: "4" }, { mnemonic: "push", price: "4" }];
       lvl.playerHeroName = "rezkar";
       lvl.playerLife = "";
       lvl.playerCards = [];
@@ -129,7 +129,7 @@ export class GameConfigurationModel {
       lvl.lvlName = "lvl10";
       lvl.botHeroName = "bot10";
       lvl.botLife = "120";
-      lvl.botCards = [{ mnemonic: "worm", price: "4" }, { mnemonic: "catapult", price: "4" }, { mnemonic: "c_attack", price: "4" }];
+      lvl.botCards = [{ mnemonic: "worm", price: "4" }, { mnemonic: "berserk", price: "4" }, { mnemonic: "c_attack", price: "4" }];
       lvl.playerHeroName = "rezkar";
       lvl.playerLife = "";
       lvl.playerCards = [];
